320625de6418b00186b62fb864b9bf5c5a8c9d61
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:17:35 +0530

    functions

---
+++ func.py
@@ -0,0 +1,20 @@
+#FUNCTION BASIC
+
+def greatHello():
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    
+print("Execute Function Program")
+greatHello()
+print("End")
+
+def test(name, hehe):
+    print(name , "Hi")
+    print("HEllo")
+    print("HEllo")
+    print("HEllo")
+    print(hehe)
+    
+test("Rohan", "RocksOn")
