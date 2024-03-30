94f1afd1fd249dc420b9b27217072a05d5d77543
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:18:54 +0530

    match

---
+++ match.py
@@ -0,0 +1,17 @@
+#match case is new in python added in 3.2 version of Python
+
+
+#match case basic
+a = int(input("Enter number: "))
+
+match a:
+    case 1:
+        print("CASE IS 1")
+    case 2:
+        print("CASE IS 2")
+    case 3:
+        print("CASE IS 3")
+    case 4:
+        print("CASE IS 4")
+    case 5:
+        print("CASE IS 5")
