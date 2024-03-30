0bcf80c86aef7b5e9585507f5c8c709e5a34aedc
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:16:20 +0530

    directory

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+dict1 = {"good": "Something Pleasant", "Pagal": "Mad", "Chagal": "Goat"}
+
+a = input("What You want to find bro ?")
+
+print(dict1[a])
+
+# "good": "Something Pleasant" Here "good" is Keys and after ':' is values 
+
+print(dict1.keys())
+print(dict1.items())
+print(dict1.values())
