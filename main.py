a90bb7fb7980f1355fbcb728495601b44a4b8c5b
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:18:34 +0530

    Python Basic Starting Programs

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Hello World")
+
+#Variables in Python Programs
+a = 5
+print(a)
+
+b = 6.2
+print(b)
+
+c = True #True and False are boolean variables so its not a string
+print(c)
+
+d = "Rohan"
+print(d)
+
+e = None #None type data type
+print(e)
+
+#Typecasting 
+f = "6" #string
+print(f)
+
+g = 5 #int
+print(g+1)
+
+print(int(f)+1) #type casting of string to int
