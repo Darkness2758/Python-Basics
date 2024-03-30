2a1b4f00e5d380698f77c16cb5c95f828c25445d
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:20:01 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,37 @@
+num1 = 345
+num2 = 2
+
+
+# arithmetic operators
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2) #Floor division
+print(num1 ** num2) #exponensiation operator (power operator)
+print(num1 % num2)  #reminder
+
+#assignment operators
+
+a = 4   # = is assignment operator
+a += 2
+print(a)
+
+#comparison operator
+
+x = 8
+y = 3
+z = 10
+
+print(x > y)
+print(x < y)
+print(x != y)
+print(x == y)
+
+#Logical Comparison
+
+print(x==z and x<y) #And Operator
+print(x==z or x<y) #Or Operator
+print(not(False)) #Not Operator
+print(not(True)) #Not Operator
