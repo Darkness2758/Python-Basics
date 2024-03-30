d263c2dce14e9b1133610d4060cae291de6f6a69
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:17:49 +0530

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+#Bekar Time tha islye
+if(8>90):
+    print("Yes")
+else:
+    print("No")
+ 
+ #If-Else Basic 
+    
+age = int(input("Enter your age :"))
+
+if(age >= 18):
+    print("Yu can drive")
+else:
+    print("Bachcha Bhaag ye ha se")
