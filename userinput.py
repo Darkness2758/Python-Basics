8ddc1ddb5c6c14bddafd3430d9f18a5161de3cb6
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:21:44 +0530

    userinput

---
+++ userinput.py
@@ -0,0 +1,15 @@
+# name = input("Enter Your Name :")
+# print(name)
+
+# num = input("Enter a number :")
+# print(type(num))
+# print(int(num) + 6)
+
+#You have to use type casting to change it into int as the default data type of input funtion is string
+
+a1 = 'Rohan\n'
+a2 = "Rohan\n"
+a3 = '''Rohan is a 
+good boy\n'''
+
+print(a1, a2, a3)
