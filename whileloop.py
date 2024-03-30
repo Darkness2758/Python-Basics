d3f2d1f1a554d7fbd00fa2ab30292e0973fac47a
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:22:00 +0530

    whileloop

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+
+
+i=0
+
+while(i<10):
+    print(i)
+    
+    i +=1
+    
+print("Done")
+
+#While Loop in Always True Format
+
+while(True):
+    num= int(input("Enter The Number : "))
+    print(num)
+    if(num==0):
+        break
+
