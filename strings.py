6345232bbfe7449b2bcd32cd2fd36e54991ee34f
Darkness2758 <basur0521@gmail.com>
2024-03-30 12:20:45 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,16 @@
+name = 'Rohan'
+title = 'Basu'
+print(name)
+
+print(name[0:3]) #starts from 0 to 2(3-1)
+print(name[1:4]) #starts from 1 to 3(4-1)
+
+#small exploration 
+print(name.upper())
+print(name.casefold())
+print(name.center(3))
+print(name.isnumeric())
+print(name.format())
+print(name.encode())
+print(name.join(title))
+print(name.split())
